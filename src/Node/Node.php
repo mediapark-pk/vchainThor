c047ee8277fcf8d456d0fc0219ce39165d04d364
sarmad <sarmad@gmail.com>
2020-09-11 16:07:38 +0500

    NOde Update

---
+++ src/Node/Node.php
@@ -0,0 +1,43 @@
+<?php
+
+
+namespace VchainThor\Node;
+
+
+use VchainThor\Exception\VchainThorException;
+use VchainThor\Vchain;
+
+/**
+ * Class Node
+ * @package VchainThor\Node
+ */
+class Node
+{
+    /*** @var Vchain */
+    public Vchain $vchain;
+
+
+    /**
+     * Node constructor.
+     * @param Vchain $vchain
+     */
+    public function __construct(Vchain $vchain)
+    {
+        $this->vchain = $vchain;
+    }
+
+    /**
+     * @return array
+     */
+    public function getConnectedPeers(): array
+    {
+        $endpoint = "/node/network/peers";
+        $response = $this->vchain->callToCurl($endpoint);
+        if ($response->payload()) {
+            return $response->payload()->array();
+        } else {
+            throw new VchainThorException("Nothing Found");
+        }
+    }
+
+}
